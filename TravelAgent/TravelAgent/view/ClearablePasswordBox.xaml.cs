093294a411cfcdf74982340791e916bab4b3f70b
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-18 10:53:32 +0200

    Added Responsive TextBoxes

---
+++ TravelAgent/TravelAgent/view/ClearablePasswordBox.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravelAgent.view
+{
+    /// <summary>
+    /// Interaction logic for ClearablePasswordBox.xaml
+    /// </summary>
+    public partial class ClearablePasswordBox : UserControl
+    {
+        public ClearablePasswordBox()
+        {
+            InitializeComponent();
+        }
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+
+        }
+
+
+        private void txtInput_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Password))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
