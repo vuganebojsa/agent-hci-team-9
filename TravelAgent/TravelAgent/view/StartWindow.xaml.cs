e7fcfcf9c2e141a402a2b95f410bc3dc474ab41b
Bogdan <bogdanjanosevic@gmail.com>
2023-05-20 20:33:10 +0200

    Created new start window

---
+++ TravelAgent/TravelAgent/view/StartWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelAgent.view
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_ButtonClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow = new Login();
+
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+        private void Register_ButtonClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow = new Registration();
+
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+
+        private void Putovanja_ButtonClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow = new Registration();
+
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+    }
+}
