427eeaf1bb062c31e1ad0be79d61f2b612407a9f
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-23 19:39:23 +0200

    Added basic logic for add/edit restaurants

---
+++ TravelAgent/TravelAgent/view/AddPlaceRestaurantPopup.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelAgent.Model;
+
+namespace TravelAgent.view
+{
+    /// <summary>
+    /// Interaction logic for AddPlaceRestaurantPopup.xaml
+    /// </summary>
+    public partial class AddPlaceRestaurantPopup : Window
+    {
+        public PlaceRestaurant PlaceRestaurant { get; set; }
+        public AddPlaceRestaurantPopup()
+        {
+            InitializeComponent();
+        }
+
+        public AddPlaceRestaurantPopup(PlaceRestaurant placeRestaurant)
+        {
+            this.PlaceRestaurant = placeRestaurant;
+            if(this.PlaceRestaurant != null)
+            {
+                tbRegistration.Text = "Izmenite smestaj ili restoran";
+                FillFields();
+            }
+            InitializeComponent();
+        }
+
+          
+        private void FillFields()
+        {
+            //    ComboBox1.Items.RemoveAt(ComboBox1.Items.IndexOf(ComboBox1.SelectedItem));  
+
+        }
+
+ 
+
+        private void btnCancel_ButtonClicked(object sender, EventArgs e)
+        {
+            YesNoPopup yn = new YesNoPopup("Da li ste sigurni da zelite da otkazete? Ovom akcijom nista nece biti izmenjeno.");
+            double width = Window.GetWindow(this).Width;
+            double height = Window.GetWindow(this).Height;
+            double left = Window.GetWindow(this).Left;
+            double top = Window.GetWindow(this).Top;
+            yn.Left = left + width / 2 - 100;
+            yn.Top = top + height / 2 - 100;
+
+            if (yn.ShowDialog() == true)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnSave_ButtonClicked(object sender, EventArgs e)
+        {
+            Vrsta type = (Vrsta)cbType.SelectedItem;
+            String name = tbNaziv.Text.Trim();
+            String mesto = tbMesto.Text.Trim();
+
+
+
+
+
+            // da bi mogli znati da li je sacuvano da se refreshuje tabela
+            this.DialogResult = true;
+
+
+
+        }
+    }
+}
