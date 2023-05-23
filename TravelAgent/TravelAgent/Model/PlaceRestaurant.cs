ebe45cf9d670d630e8fd588b9ecb73fd8183d05d
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-23 14:48:41 +0200

    Added classes

---
+++ TravelAgent/TravelAgent/Model/PlaceRestaurant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgent.Model
+{
+    public class PlaceRestaurant
+    {
+
+        public long Id { get; set; }
+        public String Naziv { get; set; }
+        public String Adresa { get; set; }
+        public Vrsta vrsta { get; set; }
+
+        public PlaceRestaurant()
+        {
+
+        }
+        public PlaceRestaurant(long id, string name, string address, Vrsta vrsta)
+        {
+            Id = id;
+            Naziv = name;
+            this.Adresa = address;
+            this.vrsta = vrsta; 
+        }
+    }
+
+    public enum Vrsta
+    {
+        Restoran, Smestaj
+    }
+}
---
+++ TravelAgent/TravelAgent/Model/Trip.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgent.Model
+{
+    public class Trip
+    {
+        public long Id { get; set; }
+        public String Naziv { get; set; }   
+        public double Cena { get; set; }
+        public DateTime DatumPocetka { get; set; }
+        public DateTime DatumKraja { get; set; }
+
+        public List<TouristAttraction> Atrakcije { get; set; }
+        public List<PlaceRestaurant> SmestajRestorani { get; set; }
+
+        public Trip(long id, string naziv, double cena, DateTime datumPocetka, DateTime datumKraja, List<TouristAttraction> atrakcije, List<PlaceRestaurant> smestajRestorani)
+        {
+            Id = id;
+            Naziv = naziv;
+            Cena = cena;
+            DatumPocetka = datumPocetka;
+            DatumKraja = datumKraja;
+            Atrakcije = atrakcije;
+            SmestajRestorani = smestajRestorani;
+        }
+        public Trip(string naziv, double cena, DateTime datumPocetka, DateTime datumKraja)
+        {
+            Naziv = naziv;
+            Cena = cena;
+            DatumPocetka = datumPocetka;
+            DatumKraja = datumKraja;
+            Atrakcije = new List<TouristAttraction>();
+            SmestajRestorani = new List<PlaceRestaurant>();
+
+        }
+
+        public void AddAttraction(TouristAttraction attraction)
+        {
+            Atrakcije.Add(attraction);
+        }
+
+        public void AddPlaceRestaurant(PlaceRestaurant attraction)
+        {
+            SmestajRestorani.Add(attraction);
+        }
+
+        public void RemoveAttraction(long id)
+        {
+            foreach(TouristAttraction att in Atrakcije)
+            {
+                if(att.Id == id)
+                {
+                    Atrakcije.Remove(att);
+                    break;
+                }
+            }
+        }
+
+        public void RemovePlaceRestaurant(long id)
+        {
+            foreach (PlaceRestaurant att in SmestajRestorani)
+            {
+                if (att.Id == id)
+                {
+                    SmestajRestorani.Remove(att);
+                    break;
+                }
+            }
+        }
+    }
+}
